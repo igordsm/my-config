6fbb1c9c2e3418efbd9c294a67c3adc74352dbaa
Igor dos Santos Montagner <Igor Montagner>
2026-05-19 15:18:10 -0300

    updates using fzf-lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,10 @@
+vim.g.mapleader = ','
+
 vim.pack.add({
   'https://github.com/navarasu/onedark.nvim',
   'https://github.com/folke/which-key.nvim',
   'https://github.com/github/copilot.vim',
+  'https://github.com/ibhagwan/fzf-lua',
   'https://github.com/neovim/nvim-lspconfig',
 })
 
@@ -15,9 +18,19 @@ if os.getenv("TERMUX") ~= nil then
  vim.o.background = "light"
 end
 
+require('fzf-lua').setup { 'default' }
+
+
+----- Key bindings
+
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
 vim.keymap.set("n", "<Up>", "gk")
 vim.keymap.set("n", "<Down>", "gj")
+
+vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>")
+vim.keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<CR>")
+vim.keymap.set("n", "<leader>p", "<cmd>FzfLua global<CR>")
+
 
 
 --- LSP stuff
